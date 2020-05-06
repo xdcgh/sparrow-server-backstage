8026a6662ee40aa002807e3884fdc342512a80ea
xdc <1434412784@qq.com>
2020-05-06 20:48:05 +0800

    fix: 继续修复跨域的问题

--- src/main/java/com/xdc/sparrowShop/config/CORSConfiguration.java
+++ src/main/java/com/xdc/sparrowShop/config/CORSConfiguration.java
@@ -12,18 +12,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class CORSConfiguration {
     @Bean
-    public WebMvcConfigurer CORSConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
+            //重写父类提供的跨域请求处理的接口
             public void addCorsMappings(CorsRegistry registry) {
+                //添加映射路径
                 registry.addMapping("/**")
+                        //放行哪些原始域
                         .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        //设置是否允许跨域传cookie
+                        //是否发送Cookie信息
                         .allowCredentials(true)
-                        //设置缓存时间，减少重复响应
-                        .maxAge(3600);
+                        //放行哪些原始域(请求方式)
+                        .allowedMethods("GET","POST", "PUT", "DELETE")
+                        //放行哪些原始域(头部信息)
+                        .allowedHeaders("*")
+                        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                        .exposedHeaders("Header1", "Header2");
             }
         };
     }
